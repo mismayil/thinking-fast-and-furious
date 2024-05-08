89d297fab3a8a94c18a7bb6e0a7341c187802af2
Arina Rak <arina.ruck@gmail.com>
2024-05-08 19:44:18 +0200

    make report.sh reusable

--- baseline/report.sh
+++ baseline/report.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 prediction_path=$1
-python drivelm/challenge/evaluation.py --root_path1 $prediction_path --root_path2 /home/rak/thinking-fast-and-furious/drivelm/challenge/test.json
+python drivelm/challenge/evaluation.py --root_path1 $prediction_path --root_path2 drivelm/challenge/test.json
