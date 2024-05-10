cd42596513df34fc30b2914ceb39becf1437eec3
Mete Ismayilzada <mismayilza@gmail.com>
2024-05-11 00:37:08 +0200

    add redcircle vb results

--- experiments/baseline/prepare_data.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-input_file=...
-
-# python ../drivelm/challenge/extract_data.py -i ${input_file} # => output written to input_file with _ext
-# python ../drivelm/challenge/convert2llama.py -i ${input_file}
