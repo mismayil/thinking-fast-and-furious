8114328af76148dddeea810ceba263a12f070ba0
Mete Ismayilzada <mismayilza@gmail.com>
2024-05-20 15:47:19 +0200

    updates for training with chained context

--- experiments/redcircle-vb/finetune.py
+++ experiments/redcircle-vb/finetune.py
@@ -1,20 +1,21 @@
 import wandb
-import torch
-from peft import LoraConfig
-from transformers import AutoProcessor, BitsAndBytesConfig, Idefics2ForConditionalGeneration
-import os
-from datasets import load_dataset
+from transformers import AutoProcessor
+import pathlib
 from transformers import TrainingArguments, Trainer
+from datasets import load_dataset
 
-from modeling import process_dataset, produce_idefics_dataset, GVQADataCollator
+from modeling import process_dataset, produce_idefics_dataset, GVQADataCollator, load_model
 
-os.environ["HF_HOME"] = "/mnt/nlpdata1/home/ismayilz/.cache/huggingface"
+MNT_POINT = "/mnt/u14157_ic_nlp_001_files_nfs"
+
+if not pathlib.Path(MNT_POINT).exists():
+    MNT_POINT = "/mnt"
 
 DEVICE = "cuda:0"
 USE_LORA = False
 USE_QLORA = True
 
-with open("/mnt/nlpdata1/home/ismayilz/.wandb.key", "r") as f:
+with open(f"{MNT_POINT}/nlpdata1/home/ismayilz/.wandb.key", "r") as f:
     wandb_key = f.read().strip()
     
 wandb.login(key=wandb_key)
@@ -24,17 +25,10 @@ wandb.init(
 wandb_name=wandb.run.name
 
 if __name__ == "__main__":
-    # Three options for training, from the lowest precision training to the highest precision training:
-    # - QLora
-    # - Standard Lora
-    # - Full fine-tuning
-    train_data_path = '/mnt/nlpdata1/home/ismayilz/cs503-project/data/train/nuscenes/v1_1_train_nus_ext_converted.json'
-    test_data_path = '/mnt/nlpdata1/home/ismayilz/cs503-project/thinking-fast-and-furious/drivelm/challenge/test_eval.json'
-    idefics_train_data_path = '/mnt/nlpdata1/home/ismayilz/cs503-project/thinking-fast-and-furious/experiments/redcircle-vb/data/nuscenes/train_idefics_redcircle_vb.json'
-    idefics_test_data_path = '/mnt/nlpdata1/home/ismayilz/cs503-project/thinking-fast-and-furious/experiments/redcircle-vb/data/nuscenes/test_idefics_redcircle_vb.json'
-    # checkpoint_dir = f"/home/cchang/CS503_VisualIntelligence/thinking-fast-and-furious/baseline/experiments/eea/models/idefics_redcircle/{wandb_name}"
-    # checkpoint_dir = "/mnt/nlpdata1/home/ismayilz/cs503-project/models/idefics2-redcircle-prime-music-8-500step/checkpoint-500"
-    checkpoint_dir = "/mnt/nlpdata1/home/ismayilz/cs503-project/models/idefics2-redcircle-vb"
+    train_data_path = f"{MNT_POINT}/nlpdata1/home/ismayilz/cs503-project/data/train/nuscenes/v1_1_train_nus_ext_converted.json"
+    test_data_path = f"{MNT_POINT}/nlpdata1/home/ismayilz/cs503-project/thinking-fast-and-furious/drivelm/challenge/test_eval.json"
+    idefics_train_data_path = f"{MNT_POINT}/nlpdata1/home/ismayilz/cs503-project/thinking-fast-and-furious/experiments/redcircle-vb/data/nuscenes/train_idefics_redcircle_vb_chain.json"
+    checkpoint_dir = f"{MNT_POINT}/nlpdata1/home/ismayilz/cs503-project/models/idefics2-redcircle-vb-chain"
     model_dir = "HuggingFaceM4/idefics2-8b"
     
     processor = AutoProcessor.from_pretrained(
@@ -42,43 +36,11 @@ if __name__ == "__main__":
         do_image_splitting=False
     )
 
-    if USE_QLORA or USE_LORA:
-        lora_config = LoraConfig(
-            r=8,
-            lora_alpha=8,
-            lora_dropout=0.1,
-            target_modules='.*(text_model|modality_projection|perceiver_resampler).*(down_proj|gate_proj|up_proj|k_proj|q_proj|v_proj|o_proj).*$',
-            use_dora=False if USE_QLORA else True,
-            init_lora_weights="gaussian"
-        )
-        if USE_QLORA:
-            bnb_config = BitsAndBytesConfig(
-                load_in_4bit=True,
-                bnb_4bit_quant_type="nf4",
-                bnb_4bit_compute_dtype=torch.float16
-            )
-        model = Idefics2ForConditionalGeneration.from_pretrained(
-            model_dir,
-            torch_dtype=torch.float16,
-            quantization_config=bnb_config if USE_QLORA else None,
-        )
-        model.add_adapter(lora_config)
-        model.enable_adapters()
-    else:
-        model = Idefics2ForConditionalGeneration.from_pretrained(
-            model_dir,
-            torch_dtype=torch.float16,
-            _attn_implementation="flash_attention_2", # Only available on A100 or H100
-        ).to(DEVICE)
-    
-    train_dataset = process_dataset(train_data_path)
-    test_dataset = process_dataset(test_data_path)
-
+    model = load_model(model_dir, eval_mode=False, use_lora=USE_LORA, use_qlora=USE_QLORA, device=DEVICE)
+    train_dataset = process_dataset(train_data_path, apply_context="chain")
     train_idefics_dataset = produce_idefics_dataset(train_dataset, output_path=idefics_train_data_path)
-    test_idefics_dataset = produce_idefics_dataset(test_dataset, output_path=idefics_test_data_path)
-
-    data = load_dataset('json', data_files=idefics_train_data_path, split=None)
-    split_dataset = data['train'].train_test_split(test_size=0.025)
+    idefics_dataset = load_dataset('json', data_files=idefics_train_data_path, split=None)
+    idefics_dataset = idefics_dataset["train"].train_test_split(test_size=0.025)
 
     training_args = TrainingArguments(
         num_train_epochs=1,
@@ -96,8 +58,7 @@ if __name__ == "__main__":
         eval_steps=100,
         fp16=True,
         remove_unused_columns=False,
-        report_to="wandb",
-        run_name="idefics-8b-redcircle-vb"
+        report_to="wandb"
     )
 
     data_collator = GVQADataCollator(processor)
@@ -106,8 +67,8 @@ if __name__ == "__main__":
         model=model,
         args=training_args,
         data_collator=data_collator,
-        train_dataset=split_dataset["train"],
-        eval_dataset=split_dataset["test"], # You can also evaluate (loss) on the eval set, note that it will incur some additional GPU memory
+        train_dataset=idefics_dataset["train"],
+        eval_dataset=idefics_dataset["test"], # You can also evaluate (loss) on the eval set, note that it will incur some additional GPU memory
     )
 
     trainer.train()
