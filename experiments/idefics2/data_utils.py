e237af45591ad8f569871ee5d7ab03743b17cf73
Arina Rak <arina.ruck@gmail.com>
2024-05-28 21:03:30 +0200

    update data utils

--- experiments/idefics2/data_utils.py
+++ experiments/idefics2/data_utils.py
@@ -4,7 +4,7 @@ from PIL import Image
 import torch
 
 # tokens corresponding to '\nAssistant:' that we want to mask out when calculating the loss
-N_TOKENS_TO_MASK_AFTER_EOU = 5 
+N_TOKENS_TO_MASK_AFTER_EOU = 6
 IMAGE_SRC_X, IMAGE_SRC_Y = 1600, 900
 IMAGE_TGT_X, IMAGE_TGT_Y = int(IMAGE_SRC_X / 2.5), int(IMAGE_SRC_Y / 2.5)
 TAGGED_CHAT_TEMPLATE = "{% for message in messages %}{{message['role'].capitalize()}}{% if message['content'][0]['type'] == 'image' %}{{':'}}{% else %}{{': '}}{% endif %}{% for line in message['content'] %}{% if line['type'] == 'text' %}{{line['text']}}{% elif line['type'] == 'image' %}{{line['text']}}{{': <image> ' }}{% endif %}{% endfor %}<end_of_utterance>\n{% endfor %}{% if add_generation_prompt %}{{ 'Assistant:' }}{% endif %}"
@@ -36,6 +36,7 @@ class GVQADataCollator:
         self.end_of_utterance_id = processor.tokenizer.additional_special_tokens_ids[
             processor.tokenizer.additional_special_tokens.index("<end_of_utterance>")
         ]
+        self.pad_token_id = processor.tokenizer.pad_token_id
 
         self.chat_template = processor.chat_template
         if chat_template == 'tagged':
@@ -43,12 +44,20 @@ class GVQADataCollator:
 
 
     def _build_chat_template_mask(self, input_ids):
+        """
+        masking out the tokens related to the user message and system tokens in the chat template 
+        """
         batch_size, seq_length = input_ids.size()
         first_end_of_utterance_ids = (input_ids == self.end_of_utterance_id).nonzero()[::2, 1] +  N_TOKENS_TO_MASK_AFTER_EOU
         mask_range = torch.arange(seq_length).unsqueeze(0).expand(batch_size, seq_length)
         mask = mask_range < first_end_of_utterance_ids.unsqueeze(-1)
-        # masking the last <EOU> token
-        #mask[:, -1] = True
+        return mask
+    
+    def _build_pad_mask(self, input_ids):
+        """
+        masking out the tokens related to the user message and system tokens in the chat template 
+        """
+        mask = input_ids == self.pad_token_id
         return mask
 
     def __call__(self, examples):
@@ -72,7 +81,7 @@ class GVQADataCollator:
         batch = self.processor(text=texts, images=images, return_tensors="pt", padding=True)
         labels = batch["input_ids"].clone()
         # added user message masking 
-        labels_mask = self._build_chat_template_mask(batch["input_ids"])
+        labels_mask = self._build_pad_mask(batch["input_ids"])
         labels[labels_mask] = self.image_token_id
         batch["labels"] = labels
 
